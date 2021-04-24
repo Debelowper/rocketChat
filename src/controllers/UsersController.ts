b2f48e6e6663500e446da982afbaecb05d76273d
Guilherme <guilherme.below@dayback.com.br>
2021-04-24 12:28:12 -0300

    fourth day complete

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,5 +1,5 @@
 import { Request, Response} from  "express"
-import { UsersService } from "../Service/UsersService"
+import { UsersService } from "../services/UsersService"
 
 class UsersController {
   async create(request: Request, response: Response): Promise<Response>{
